e6b66228c626d83412d2f1ac25b3047f5e7417bd
Oscaross <44754250+Oscaross@users.noreply.github.com>
2025-09-23 15:01:55 +0000

    Initial Commit

---
+++ EatNeat/EatNeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatNeatApp.swift
+//  EatNeat
+//
+//  Created by Oscar Horner on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct EatNeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EatNeatTests/EatNeatTests.swift
@@ -0,0 +1,17 @@
+//
+//  EatNeatTests.swift
+//  EatNeatTests
+//
+//  Created by Oscar Horner on 23/09/2025.
+//
+
+import Testing
+@testable import EatNeat
+
+struct EatNeatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
